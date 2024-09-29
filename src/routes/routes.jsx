1c500d71e8ec7cd11bebd9e5b3d0dad3f2ea21df
tahsin soufizade <soufilearn@gmail.com>
2024-09-29 14:05:06 +0330

    add needed routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,12 @@
 import Home from "../pages/home/Home";
+import NewUser from "../pages/newUser/NewUser";
+import Products from "../pages/products/Products";
+import UsersList from "../pages/users/UsersList";
 
-const routes = [{ path: "/", element: <Home /> }];
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/users", element: <UsersList /> },
+  { path: "/newUser", element: <NewUser /> },
+  { path: "/products", element: <Products /> },
+];
 export default routes;
