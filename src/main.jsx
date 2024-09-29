c197c3cc16a7d90de74510da1a332c27d49c0cee
tahsin soufizade <soufilearn@gmail.com>
2024-09-29 14:23:16 +0330

    run tailwindCss

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./css/index.css";
+
 import { BrowserRouter as Router } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
