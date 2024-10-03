c5eabc9db4a27929c15fe384715295f4f7b56f31
tahsin soufizade <soufilearn@gmail.com>
2024-10-03 12:14:49 +0330

    change styles of items teext

--- src/components/sidebar/SidebarListItem.jsx
+++ src/components/sidebar/SidebarListItem.jsx
@@ -6,10 +6,10 @@ const SidebarListItem = ({ item }) => {
       <NavLink
         to={item.href}
         className={({ isActive }) =>
-          `flex items-center gap-2 py-3 px-4 rounded-lg relative transition-all mb-2 text-secondary-dark dark:text-secondary-light ${
+          `flex items-center gap-2 py-3 px-4 rounded-lg relative transition-all mb-2   ${
             isActive
-              ? "bg-secondary-main/20  dark:text-[#7ed6ff] text-dark-darker after:content-[''] after:absolute after:bg-secondary-main after:w-1 after:h-4/6 after:top-1/2 after:-right-[1.5px] after:transform after:-translate-y-1/2 after:rounded-md"
-              : ""
+              ? "bg-secondary-main/20 dark:text-[#7ed6ff] text-dark-darker after:content-[''] after:absolute after:bg-secondary-main after:w-1 after:h-4/6 after:top-1/2 after:-right-[1.5px] after:transform after:-translate-y-1/2 after:rounded-md"
+              : "text-secondary-dark dark:text-light-darker"
           }`
         }
       >
