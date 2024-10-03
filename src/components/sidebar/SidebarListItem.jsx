1435a680ee7edde01b3138a66557ccb1f1e3bdbe
tahsin soufizade <soufilearn@gmail.com>
2024-10-03 12:09:47 +0330

    change styles of items icon

--- src/components/sidebar/SidebarListItem.jsx
+++ src/components/sidebar/SidebarListItem.jsx
@@ -8,12 +8,12 @@ const SidebarListItem = ({ item }) => {
         className={({ isActive }) =>
           `flex items-center gap-2 py-3 px-4 rounded-lg relative transition-all mb-2 text-secondary-dark dark:text-secondary-light ${
             isActive
-              ? "bg-secondary-main/20  dark:text-[#7ed6ff] after:content-[''] after:absolute after:bg-secondary-main after:w-1 after:h-4/6 after:top-1/2 after:-right-[1.5px] after:transform after:-translate-y-1/2 after:rounded-md"
+              ? "bg-secondary-main/20  dark:text-[#7ed6ff] text-dark-darker after:content-[''] after:absolute after:bg-secondary-main after:w-1 after:h-4/6 after:top-1/2 after:-right-[1.5px] after:transform after:-translate-y-1/2 after:rounded-md"
               : ""
           }`
         }
       >
-        {item.icon} {item.label}
+        <span className="text-lg">{item.icon}</span> {item.label}
       </NavLink>
     </li>
   );
