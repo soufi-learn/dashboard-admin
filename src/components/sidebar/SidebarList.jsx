53de450268ec729410a1824d1b0bf60088090146
tahsin soufizade <soufilearn@gmail.com>
2024-10-03 12:06:02 +0330

    add more list items & change its icon

--- src/components/sidebar/SidebarList.jsx
+++ src/components/sidebar/SidebarList.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import SidebarListItem from "./SidebarListItem";
 import { HiHome } from "react-icons/hi2";
+import { IoAnalyticsOutline } from "react-icons/io5";
+import { MdOutlineAirlineStops } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa6";
 
 const SidebarList = () => {
   const [sidebarItems, setSidebarItems] = useState([
     { id: 1, label: "داشبورد", icon: <HiHome />, href: "/" },
-    { id: 2, label: "لیست سفارشات", icon: <HiHome />, href: "/orders" },
-    { id: 3, label: "مشتریان", icon: <HiHome />, href: "/customers" },
+    {
+      id: 2,
+      label: "تجزیه و تحلیل",
+      icon: <IoAnalyticsOutline />,
+      href: "/analytics",
+    },
+    {
+      id: 3,
+      label: "فروش",
+      icon: <MdOutlineAirlineStops />,
+      href: "/sale",
+    },
+    {
+      id: 4,
+      label: "کاربران",
+      icon: <FaRegUser />,
+      href: "/users",
+    },
   ]);
 
   return (
