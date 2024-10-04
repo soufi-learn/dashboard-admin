3d89fe119e362fdf376bdaa50bf5c87530eed214
tahsin soufizade <soufilearn@gmail.com>
2024-10-04 13:22:04 +0330

    add burger buttons to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import NotificationButton from "./NotificationButton";
 import DarkLightButton from "./DarkLightButton";
 import NotificationButtonsList from "./NotificationButtonsList";
+import { Button } from "@headlessui/react";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
 
 const Header = () => {
   const [notificationsList, setNotificationsList] = useState(
@@ -11,6 +13,10 @@ const Header = () => {
   return (
     <header>
       <div className="flex items-center justify-between">
+        <Button className="rounded bg-primary-main md:hidden p-2 hover:bg-primary-light text-sm text-white active:bg-primary-main dark:bg-light-main dark:text-dark-darker dark:hover:bg-light-darker dark:active:bg-light-main">
+          <HiMiniBars3BottomRight size="1.6rem" />
+        </Button>
+
         <DarkLightButton />
 
         <div className="flex items-center gap-7">
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import SidebarList from "./SidebarList";
 
 const Sidebar = () => {
   return (
-    <div className="w-1/5 min-h-[94vh] sticky top-6 p-3 transition-all rounded-lg bg-light-darker text-dark-main dark:text-light-main dark:bg-dark-lighter">
+    <div className="hidden w-1/5 min-h-[94vh] sticky top-6 p-3 transition-all rounded-lg bg-light-darker text-dark-main dark:text-light-main dark:bg-dark-lighter">
       <div className="mt-10 mb-8">
         <div className="relative mb-4 gradient-backline">
           <img
