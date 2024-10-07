cf3dd37ef9356b31118cdc5d4a61c0c4c0808dcc
tahsin soufizade <soufilearn@gmail.com>
2024-10-07 11:25:45 +0330

    change behind box backgroundColor

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { IoArrowUpOutline, IoArrowDownOutline } from "react-icons/io5";
-import { getProfitSign } from "../../helper/helper";
+import FeatureItem from "./Featureitem";
 
 const Features = () => {
   const [featuresList, setFeaturesList] = useState([
@@ -11,40 +10,7 @@ const Features = () => {
   return (
     <div className="flex gap-4">
       {featuresList.map((feature) => (
-        <section
-          key={feature.id}
-          className="rounded-md bg-white dark:bg-primary-light p-4 shadow-md transition-all flex-1"
-        >
-          <span className="mb-3 block text-dark-darker transition-all dark:text-white text-xl">
-            {feature.title}
-          </span>
-
-          <div className="flex mb-4 items-center">
-            <span className="dark:text-light-main transition-all text-xl font-bold me-6">
-              {feature.price} تومان
-            </span>
-
-            <span className=" dark:text-light-main transition-all me-3">
-              {feature.profit}
-            </span>
-
-            {getProfitSign(feature.profit) === "-" ? (
-              <IoArrowDownOutline
-                className="transition-all text-red-500 dark:text-red-400"
-                size="1.4rem"
-              />
-            ) : (
-              <IoArrowUpOutline
-                className="transition-all text-green-600 dark:text-green-500"
-                size="1.4rem"
-              />
-            )}
-          </div>
-
-          <span className="text-sm text-dark-lighter dark:text-light-lighter transition-all">
-            نسبت به ماه گذشته
-          </span>
-        </section>
+        <FeatureItem key={feature.id} feature={feature} />
       ))}
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Features from "../../components/features/Features";
 const Home = () => {
   return (
     <div className="mt-10">
-      <div className="bg-secondary-main/10 dark:bg-secondary-dark/45 transition-all absolute right-[10%] -z-10 w-[90%] h-40"></div>
+      <div className="bg-secondary-main/30 dark:bg-primary-dark transition-all absolute right-[10%] -z-10 w-[90%] h-40"></div>
       <h2 className="mb-6 text-dark-main dark:text-white transition-all pt-7">
         داشبورد
       </h2>
