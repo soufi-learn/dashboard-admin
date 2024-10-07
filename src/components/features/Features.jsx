570122070adab33f22124dec84693510380e365a
tahsin soufizade <soufilearn@gmail.com>
2024-10-07 11:36:35 +0330

    change behind box backgroundColor

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -3,9 +3,9 @@ import FeatureItem from "./FeatureItem";
 
 const Features = () => {
   const [featuresList, setFeaturesList] = useState([
-    { id: 1, title: "درآمد", price: "۴۸,۶۸۰,۰۰۰", profit: "۲۴.۵ -" },
-    { id: 2, title: "درآمد", price: "۴۸,۶۸۰,۰۰۰", profit: "۲۴.۵ +" },
-    { id: 3, title: "درآمد", price: "۴۸,۶۸۰,۰۰۰", profit: "۲۴.۵ -" },
+    { id: 1, title: "درآمد", price: "۴۸,۶۸۰,۰۰۰", profit: "۴۴ +" },
+    { id: 2, title: "فروش", price: "۲۹,۰۰۰,۰۰۰", profit: "۱۷.۵ +" },
+    { id: 3, title: "هزینه ها", price: "۱۵,۲۰۰,۰۰۰", profit: "۳۴.۲ -" },
   ]);
   return (
     <div className="flex gap-4">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,8 @@ import Features from "../../components/features/Features";
 const Home = () => {
   return (
     <div className="mt-10">
-      <div className="bg-secondary-main/30 dark:bg-primary-dark transition-all absolute right-[10%] -z-10 w-[90%] h-40"></div>
-      <h2 className="mb-6 text-dark-main dark:text-white transition-all pt-7">
-        داشبورد
-      </h2>
+      <div className="bg-secondary-main dark:bg-primary-dark transition-all absolute right-[10%] -z-10 w-[90%] h-40"></div>
+      <h2 className="mb-6 text-white pt-7">داشبورد</h2>
       <Features />
     </div>
   );
