0fc880424aecb2f6a943d5390481a08b4440c4e1
tahsin soufizade <soufilearn@gmail.com>
2024-10-07 11:19:47 +0330

    add box behind main title

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,8 @@ import Features from "../../components/features/Features";
 const Home = () => {
   return (
     <div className="mt-10">
-      <h2 className="mb-6 text-dark-main dark:text-white transition-all">
+      <div className="bg-secondary-main/10 dark:bg-secondary-dark/45 transition-all absolute right-[10%] -z-10 w-[90%] h-40"></div>
+      <h2 className="mb-6 text-dark-main dark:text-white transition-all pt-7">
         داشبورد
       </h2>
       <Features />
