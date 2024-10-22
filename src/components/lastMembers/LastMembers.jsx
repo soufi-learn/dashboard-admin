3e3f875a2c415cf53540390f3a51e17072c1ca8e
tahsin soufizade <soufilearn@gmail.com>
2024-10-22 12:33:36 +0330

    set dynamic classes to status buttons

--- src/components/lastMembers/LastMembers.jsx
+++ src/components/lastMembers/LastMembers.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+
 const LastMembers = () => {
+  // تابع برای گرفتن کلاس بر اساس وضعیت
+  const getButtonColor = (status) => {
+    switch (status) {
+      case "approve":
+        return "bg-green-200 text-green-700 hover:bg-green-300 hover:text-green-900 transition-all";
+      case "decline":
+        return "bg-red-200 text-red-700 hover:bg-red-300 hover:text-red-900 transition-all";
+      case "pending":
+        return "bg-blue-200 text-blue-700 hover:bg-blue-300 hover:text-blue-900 transition-all";
+      default:
+        return "bg-gray-200 text-gray-700 hover:bg-gray-300 hover:text-gray-900 transition-all";
+    }
+  };
+  const getButtonText = (status) => {
+    switch (status) {
+      case "approve":
+        return "تایید شد";
+      case "decline":
+        return "رد شد";
+      case "pending":
+        return "در انتظار";
+      default:
+        return "نامعلوم";
+    }
+  };
+
+  const Button = ({ type }) => {
+    return (
+      <button className={`px-4 py-1 rounded-md ${getButtonColor(type)}`}>
+        {getButtonText(type)}
+      </button>
+    );
+  };
+
   return (
     <div className="flex-[2]">
       <h3 className="mb-6 transition-all text-dark-darker dark:text-white">
@@ -19,7 +55,9 @@ const LastMembers = () => {
             <td className="p-2 text-center border">تحسین صوفی زاده</td>
             <td className="p-2 text-center border">۱۳۸۱/۰۹/۲۲</td>
             <td className="p-2 text-center border">۲,۴۸۰,۰۰۰ تومان</td>
-            <td className="p-2 text-center border">پرداخت شده</td>
+            <td className="p-2 text-center border">
+              <Button type="approve" />
+            </td>
           </tr>
         </tbody>
       </table>
