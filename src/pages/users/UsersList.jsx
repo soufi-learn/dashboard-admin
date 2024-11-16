983c37dde57aaccd1ce17e0bb4fc008aa1270cb1
tahsin soufizade <soufilearn@gmail.com>
2024-11-16 09:09:27 +0330

    handle delete users

--- src/pages/users/UsersList.jsx
+++ src/pages/users/UsersList.jsx
@@ -1,32 +1,37 @@
 import { useState } from "react";
 import usersList from "../../data/usersList";
+import { Link } from "react-router-dom";
 
 const Users = () => {
   const [usersData, setUsersData] = useState(usersList);
 
+  const userDelete = (id) => {
+    setUsersData(usersData.filter((user) => user.id !== id));
+  };
+
   return (
     <div className="mt-20">
-      <table class="table-fixed">
+      <table className="table-fixed">
         <thead>
           <tr></tr>
         </thead>
         <tbody></tbody>
       </table>
 
-      <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class=" text-dark-darker uppercase bg-light-darker transition-all dark:bg-dark-darker dark:text-light-darker">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+          <thead className="uppercase transition-all text-dark-darker bg-light-darker dark:bg-dark-darker dark:text-light-darker">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 id
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 نام کاربر{" "}
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 معاملات
               </th>
-              <th scope="col" class="px-6 py-3 text-center">
+              <th scope="col" className="px-6 py-3 text-center">
                 عملیات
               </th>
             </tr>
@@ -35,10 +40,10 @@ const Users = () => {
             {usersData.map((user) => (
               <tr
                 key={user.id}
-                class="bg-white border-b dark:bg-dark-lighter transition-all dark:border-dark-darker"
+                className="transition-all bg-white border-b dark:bg-dark-lighter dark:border-dark-darker"
               >
-                <td class="px-6 py-4">{user.id}</td>
-                <td class="px-6 py-4 flex  items-center gap-3">
+                <td className="px-6 py-4">{user.id}</td>
+                <td className="flex items-center gap-3 px-6 py-4">
                   <img
                     src={user.avatar}
                     alt={user.username}
@@ -46,13 +51,18 @@ const Users = () => {
                   />
                   {user.username}
                 </td>
-                <td class="px-6 py-4">{user.transactions}</td>
-                <td class="px-6 py-4 ">
+                <td className="px-6 py-4">{user.transactions}</td>
+                <td className="px-6 py-4 ">
                   <div className="flex items-center justify-center gap-4">
-                    <button className="px-4 py-2 text-white transition-all bg-green-500 rounded-md hover:bg-green-600">
-                      ادیت
-                    </button>
-                    <button className="px-4 py-2 text-white transition-all bg-red-500 rounded-md hover:bg-red-600">
+                    <Link to={`/users/${user.id}`}>
+                      <button className="px-4 py-2 text-white transition-all bg-green-500 rounded-md hover:bg-green-600">
+                        ادیت
+                      </button>
+                    </Link>
+                    <button
+                      onClick={() => userDelete(user.id)}
+                      className="px-4 py-2 text-white transition-all bg-red-500 rounded-md hover:bg-red-600"
+                    >
                       حذف
                     </button>
                   </div>
